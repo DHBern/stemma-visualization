12a063f799aca75c052b032b16fcb580d91eb8b2
Sebastian <sebastian@flicks.jetzt>
2022-05-12 18:22:18 +0200

    Add canvas element
    Everything is developed in this module for now.

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,18 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import Canvas from '$lib/Canvas.svelte';
+    import items from './items.json';
+
+</script>
+
+<section>
+<Canvas items={items.items} links={items.links} />
+</section>
+
+<style lang="scss">
+    section {
+        margin-left: auto;
+        margin-right: auto;
+        margin-top: 5rem;
+        width: 600px;
+    }
+</style>
