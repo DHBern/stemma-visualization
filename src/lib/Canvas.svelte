ec6504927149044095c91ae069ecb95cd36fc64b
Sebastian <sebastian@flicks.jetzt>
2022-05-19 14:36:30 +0200

    linting

--- src/lib/Canvas.svelte
+++ src/lib/Canvas.svelte
@@ -1,59 +1,64 @@
 <script>
-  import { scaleLinear, scaleOrdinal } from "d3-scale";
-  import { linkVertical, schemeAccent } from "d3"
-  import { extent } from "d3-array";
-  import { each } from "svelte/internal";
+	import { scaleLinear, scaleOrdinal } from 'd3-scale';
+	import { linkVertical, schemeAccent } from 'd3';
+	import { extent } from 'd3-array';
+	import { each } from 'svelte/internal';
 
-  export let width = 600;
-  export let height = 600;
-  export let circleRadius = 20;
+	export let width = 600;
+	export let height = 600;
+	export let circleRadius = 20;
 
-  /** @type { Array<Object> } */
-  export let items;
-  /** @type { Array<Object> } */
-  export let links;
+	/** @type { Array<Object> } */
+	export let items;
+	/** @type { Array<Object> } */
+	export let links;
 
-  export let colorScale = scaleOrdinal([...new Set(items.map(i => i.type))],schemeAccent);
+	export let colorScale = scaleOrdinal([...new Set(items.map((i) => i.type))], schemeAccent);
 
-  const buffer = 20;
-  const axisSpace = 50;
+	const buffer = 20;
+	const axisSpace = 50;
 
-  let xExtent = extent(items, (d) => d.x);
-  let yExtent = extent(items, (d) => d.y);
+	let xExtent = extent(items, (d) => d.x);
+	let yExtent = extent(items, (d) => d.y);
 
-  let xScale = scaleLinear()
-      .domain(xExtent)
-      .range([buffer + axisSpace, width - buffer]);
-    let yScale = scaleLinear()
-      .domain(yExtent)
-      .range([height - buffer - axisSpace, buffer]);
-
+	let xScale = scaleLinear()
+		.domain(xExtent)
+		.range([buffer + axisSpace, width - buffer]);
+	let yScale = scaleLinear()
+		.domain(yExtent)
+		.range([height - buffer - axisSpace, buffer]);
 </script>
 
 <svg {width} {height}>
-    {#each items as item}
-        {#if item.href}
-            <a href={item.href} target="_blank">
-                <circle
-                    r= {circleRadius}
-                    cx={xScale(item.x)}
-                    cy={yScale(item.y)}
-                    fill={colorScale(item.type)}
-                />
-            </a>
-        {:else}
-                <circle
-                    r= {circleRadius}
-                    cx={xScale(item.x)}
-                    cy={yScale(item.y)}
-                    fill={colorScale(item.type)}
-                />
-        {/if}
-    {/each}
-    {#each links as link}
-        {@const source = items.find(e => e.id === link.nodes[0])}
-        {@const target = items.find(e => e.id === link.nodes[1])}
-        <path d={linkVertical()({source: [xScale(source.x), yScale(source.y)], target: [xScale(target.x), yScale(target.y)]})} fill="none" stroke="black" />
-    {/each}
-
+	{#each items as item}
+		{#if item.href}
+			<a href={item.href} target="_blank">
+				<circle
+					r={circleRadius}
+					cx={xScale(item.x)}
+					cy={yScale(item.y)}
+					fill={colorScale(item.type)}
+				/>
+			</a>
+		{:else}
+			<circle
+				r={circleRadius}
+				cx={xScale(item.x)}
+				cy={yScale(item.y)}
+				fill={colorScale(item.type)}
+			/>
+		{/if}
+	{/each}
+	{#each links as link}
+		{@const source = items.find((e) => e.id === link.nodes[0])}
+		{@const target = items.find((e) => e.id === link.nodes[1])}
+		<path
+			d={linkVertical()({
+				source: [xScale(source.x), yScale(source.y)],
+				target: [xScale(target.x), yScale(target.y)]
+			})}
+			fill="none"
+			stroke="black"
+		/>
+	{/each}
 </svg>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,18 +1,17 @@
 <script>
 	import Canvas from '$lib/Canvas.svelte';
-    import items from './items.json';
-
+	import items from './items.json';
 </script>
 
 <section>
-<Canvas items={items.items} links={items.links} />
+	<Canvas items={items.items} links={items.links} />
 </section>
 
 <style lang="scss">
-    section {
-        margin-left: auto;
-        margin-right: auto;
-        margin-top: 5rem;
-        width: 600px;
-    }
+	section {
+		margin-left: auto;
+		margin-right: auto;
+		margin-top: 5rem;
+		width: 600px;
+	}
 </style>
